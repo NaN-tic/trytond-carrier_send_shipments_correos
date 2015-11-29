d7d328e2f2fcd8a0f06706308ab3d6aa020e7bc4
resteve <resteve@zikzakmedia.com>
2015-11-29 21:57:35 +0100

    pyflakes

--- manifest.py
+++ manifest.py
@@ -1,10 +1,7 @@
 # This file is part of carrier_send_shipments_correos module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from correos import Picking
 from trytond.pool import PoolMeta
-from trytond.transaction import Transaction
-from base64 import decodestring
 
 __all__ = ['CarrierManifest']
 __metaclass__ = PoolMeta
