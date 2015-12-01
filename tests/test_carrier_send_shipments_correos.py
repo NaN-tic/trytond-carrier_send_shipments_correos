17d33a2fbde88ada4293cf0e5dadeaa507781d52
resteve <resteve@zikzakmedia.com>
2015-12-01 11:04:45 +0100

    Tests

--- tests/test_carrier_send_shipments_correos.py
+++ tests/test_carrier_send_shipments_correos.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the carrier_send_shipments_correos module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class CarrierSendShipmentsCorreosTestCase(unittest.TestCase):
+class CarrierSendShipmentsCorreosTestCase(ModuleTestCase):
     'Test Carrier Send Shipments Correos module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('carrier_send_shipments_correos')
-
-    def test0005views(self):
-        'Test views'
-        test_view('carrier_send_shipments_correos')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'carrier_send_shipments_correos'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         CarrierSendShipmentsCorreosTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
