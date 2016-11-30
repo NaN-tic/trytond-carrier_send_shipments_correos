a20312c90c4eee18bed5c2cc34458d7b543dc0e5
Raimon Esteve <raimon@nan-tic.com>
2016-11-30 23:17:24 +0100

    Replace test_connection to class method

--- api.py
+++ api.py
@@ -10,7 +10,7 @@ try:
     from correos.picking import *
 except ImportError:
     logger = logging.getLogger(__name__)
-    message = 'Install Correos from Pypi: pip install correos'
+    message = 'Install Correos: pip install correos'
     logger.error(message)
     raise Exception(message)
 
@@ -22,16 +22,14 @@ class CarrierApi:
     __name__ = 'carrier.api'
     correos_code = fields.Char('Code', states={
             'required': Eval('method') == 'correos',
-            }, help='Correos Code (CodeEtiquetador)')
+        }, help='Correos Code (CodeEtiquetador)')
     correos_cc = fields.Char('CC', states={
             'required': Eval('method') == 'correos',
-            }, help='Correos Bank Number')
+        }, help='Correos Bank Number')
 
     @classmethod
     def get_carrier_app(cls):
-        '''
-        Add Carrier Correos APP
-        '''
+        'Add Carrier Correos APP'
         res = super(CarrierApi, cls).get_carrier_app()
         res.append(('correos', 'Correos'))
         return res
@@ -43,13 +41,11 @@ class CarrierApi:
                     'invisible': Not(Equal(Eval('method'), 'correos')),
                     })]
 
-    def test_correos(self, api):
-        '''
-        Test Correos connection
-        :param api: obj
-        '''
+    @classmethod
+    def test_correos(cls, api):
+        'Test Correos connection'
         message = 'Connection unknown result'
-        
+
         with API(api.username, api.password, api.correos_code, api.debug) as correos_api:
             message = correos_api.test_connection()
-        self.raise_user_error(message)
+        cls.raise_user_error(message)
